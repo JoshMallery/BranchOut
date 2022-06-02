ee8f05b36a6006d4b4c05b82a2cda8c6b8469495
Josh Mallery <josh.mallery@gmail.com>
2022-06-01 21:25:16 -0600

    Make Route for Individual Courses

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Dashboard from './components/Dashboard';
+import Course from "./components/Course";
+import Lessons from "./components/Lessons";
 import Form from './components/Form';
 import {apiCalls} from './apiCalls/apiCalls'
 
@@ -19,6 +21,12 @@ class App extends Component {
     .then(res => this.setState({courses: res}))
   }
 
+  displayCourse = (match) => {
+    const courseName = match.params.course.split("-").join(" ");
+    const matchedCourse = this.state.courses.find(course => course.title === courseName);
+    return matchedCourse
+  }
+
 render() {
   return (
     <main className='App'>
@@ -28,6 +36,16 @@ render() {
         <Route exact path="/" render={ () => !this.state.courses.length ? <h2>Loading</h2> : <Dashboard courses={this.state.courses}/> }
         />
         <Route path="/form" render={ () => <Form /> }
+        />
+        <Route path="/:course" render={({ match }) => {
+          return(
+            <div>
+              <Course {...this.displayCourse(match)} />
+              <Lessons {...this.displayCourse(match)} />
+            </div>
+            )
+          }
+        }
         />
       </Switch>
     </main>
---
+++ src/components/Course.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../CSS/Course.css";
+
+const Course = () => {
+
+  return (
+    <div>
+    I am a course!
+    </div>
+  )
+
+}
+
+export default Course
